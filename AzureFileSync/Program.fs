c008d3bd91e4ec1d29e83f63fefe025ffd4290e9
Simon Pedersen <mail@sjkp.dk>
2014-06-14 14:50:49 +0200

    removed mutable, changed to Observables

--- AzureFileSync/Program.fs
+++ AzureFileSync/Program.fs
@@ -9,13 +9,16 @@ open System.Configuration
 open System.Threading.Tasks;
 open System.Linq
 
-
-type EventType = File | Directory 
+type SynchronizingEvent = {Filename: string}
+type ItemType = File | Directory 
+type EventType = Changed | Renamed | Added | Deleted | Ignore
 type FileSyncAction = Nothing | UpdateLocalFile
 type FileShareToLocalDirMap = {ShareName : string; LocalFolder: string}
 type AzureFileSyncOptions = {map: FileShareToLocalDirMap; fs: FileSystemWatcher}
 
-let printFunc (eventType) (event : FileSystemEventArgs) = printfn "%s %s" eventType event.Name
+type FileEvent = {event: SynchronizingEvent; eventType : EventType; time: DateTime}
+
+let printFunc (event : FileEvent) = printfn "%s %A" event.event.Filename event.eventType
 let datetimeFormat = "dd.MM.yyyy HH:mm:ss"   
 
 
@@ -62,34 +65,35 @@ let createDirectory (filePath : string) (map : FileShareToLocalDirMap) =
     mkdir dirReference paths
 
 
-let disableFsAndRun (f : unit -> unit) (path : string) = 
-    ignoreFiles <- path :: ignoreFiles 
-    f()
-    let a = async {
-            do! Async.Sleep(5000)
-//            ignoreFiles <- 
-            ignoreFiles <- ignoreFiles |> Seq.filter (fun x -> x = path) |> Seq.toList
-        }
-    Async.RunSynchronously(a) 
+//let disableFsAndRun (f : unit -> unit) (path : string) = 
+//    ignoreFiles <- path :: ignoreFiles 
+//    f()
+//    let a = async {
+//            do! Async.Sleep(5000)
+////            ignoreFiles <- 
+//            ignoreFiles <- ignoreFiles |> Seq.filter (fun x -> x = path) |> Seq.toList
+//        }
+//    Async.RunSynchronously(a) 
 
-let rec mkdirLocal baseDir path = 
+let rec mkdirLocal baseDir path (event : Event<SynchronizingEvent>) = 
     match path with
     | [] -> ()
     | _ -> 
         let dirPath = System.IO.Path.Combine(baseDir,path.Head)
         match System.IO.Directory.Exists(dirPath) with   
         | false -> 
-            let f = (fun ()-> System.IO.Directory.CreateDirectory(dirPath) |> ignore)
-            disableFsAndRun f dirPath    
+            event.Trigger({Filename = dirPath})
+            System.IO.Directory.CreateDirectory(dirPath) |> ignore
+            event.Trigger({Filename = dirPath})
         | true -> ()
-        mkdirLocal dirPath path.Tail
+        mkdirLocal dirPath path.Tail event
 
 let getCloudItemShareRelativeUriParts (item : File.IListFileItem) =
     item.Uri.ToString().[item.Share.Uri.ToString().Length..].Split([|'/'|],StringSplitOptions.RemoveEmptyEntries) |> Seq.toList
 
-let syncDirectory (cd : File.CloudFileDirectory) (map : AzureFileSyncOptions) =        
+let syncDirectory (cd : File.CloudFileDirectory) (map : AzureFileSyncOptions) event =        
     let uriParts = cd |> getCloudItemShareRelativeUriParts 
-    mkdirLocal map.map.LocalFolder uriParts
+    mkdirLocal map.map.LocalFolder uriParts event
 
 let getFileSyncAction path (cf : File.CloudFile) = 
     match path |> System.IO.File.Exists with
@@ -97,51 +101,69 @@ let getFileSyncAction path (cf : File.CloudFile) =
         let fileInfo = new System.IO.FileInfo(path)        
         let cfTime = cf.Properties.LastModified.Value.ToUniversalTime()
         let locTime = new DateTimeOffset(fileInfo.LastWriteTimeUtc)
-        printfn "cloudFile LastUpdated %s localFile LastUpdate %s" (cfTime.ToString(datetimeFormat)) (locTime.ToString(datetimeFormat))
+//        printfn "cloudFile LastUpdated %s localFile LastUpdate %s" (cfTime.ToString(datetimeFormat)) (locTime.ToString(datetimeFormat))
         match cfTime > locTime with
         | true -> FileSyncAction.UpdateLocalFile
         | false -> FileSyncAction.Nothing
     | false -> FileSyncAction.UpdateLocalFile
 
     
-let syncFile (cf : File.CloudFile) (map : AzureFileSyncOptions) = 
+let syncFile (cf : File.CloudFile) (map : AzureFileSyncOptions) (event : Event<SynchronizingEvent>) = 
     let uriParts = cf |> getCloudItemShareRelativeUriParts 
     let allParts = [map.map.LocalFolder] @ uriParts |> Seq.toArray
     let path = System.IO.Path.Combine(allParts)
     match getFileSyncAction path cf with
     | FileSyncAction.Nothing -> ()
     | FileSyncAction.UpdateLocalFile -> 
-        disableFsAndRun (fun () -> cf.DownloadToFile(path, FileMode.Create)) path
+        event.Trigger({Filename = path})
+        cf.DownloadToFile(path, FileMode.Create)
+        event.Trigger({Filename = path})
+//        disableFsAndRun (fun () -> cf.DownloadToFile(path, FileMode.Create)) path
 
     
-let rec syncFilesAndDirectories (dir : File.CloudFileDirectory) map = 
+let rec syncFilesAndDirectories (dir : File.CloudFileDirectory) map event = 
     dir.ListFilesAndDirectories() |> Seq.iter((fun file -> 
         match file with
         | :? File.CloudFile as cf -> 
             cf.FetchAttributes()
-            printfn "cloud file %s %s"  cf.Name (cf.Properties.LastModified.Value.ToString(datetimeFormat))
-            syncFile cf map
+//            printfn "cloud file %s %s"  cf.Name (cf.Properties.LastModified.Value.ToString(datetimeFormat))
+            syncFile cf map event
         | :? File.CloudFileDirectory as cd -> 
             cd.FetchAttributes()
-            printfn "cloud directory %s %s" cd.Name (cd.Properties.LastModified.Value.ToString(datetimeFormat)) 
-            syncDirectory cd map
-            syncFilesAndDirectories cd map
+//            printfn "cloud directory %s %s" cd.Name (cd.Properties.LastModified.Value.ToString(datetimeFormat)) 
+            syncDirectory cd map event
+            syncFilesAndDirectories cd map event
         )) 
 
-let syncLocalFilesAndDirecotryWithAzure map = 
-    map.map.ShareName |> shareRoot |> (fun (dir) -> syncFilesAndDirectories dir map)
+let syncLocalFilesAndDirecotryWithAzure map event = 
+    map.map.ShareName |> shareRoot |> (fun (dir) -> syncFilesAndDirectories dir map event)
 
-let syncLoop map = async {
+let syncLoop map event = async {
+    
     while true do
-        syncLocalFilesAndDirecotryWithAzure map
-        do! Async.Sleep 10000
+        printfn "sync in progress"
+        syncLocalFilesAndDirecotryWithAzure map event
+        printfn "sync in done"
+        do! Async.Sleep 10000   
 }
 
-let getEventType path : EventType = 
+let getItemType path : ItemType = 
     match ((File.GetAttributes(path) &&& FileAttributes.Directory) = FileAttributes.Directory) with
-    | true -> EventType.Directory
-    | false -> EventType.File
+    | true -> ItemType.Directory
+    | false -> ItemType.File
     
+let filter l e = 
+    //printfn "filter func says: %A List contains:  %A" e l
+    let res = l |> Seq.filter(fun i -> i.eventType = EventType.Ignore) |> Seq.toList
+    match e.eventType with
+    | EventType.Ignore-> 
+        match res |> Seq.exists(fun i -> i.event.Filename = e.event.Filename) with
+        | true -> res|>Seq.filter(fun i -> i.event.Filename <> e.event.Filename) |> Seq.toList
+        | false -> e::res
+    | _ -> 
+        match res |> Seq.exists(fun i -> i.event.Filename = e.event.Filename) with
+        | true -> res
+        | false -> e::res
 
 [<EntryPoint>]
 let main argv = 
@@ -155,31 +177,44 @@ let main argv =
 
     directoryWatchers
      |> Seq.iter((fun w -> 
-     
-     w.fs.Renamed.Add((fun e -> 
-        printFunc "Renamed" e
-     ))
-     w.fs.Changed.Add((fun e -> 
-        printFunc "Changed" e
-        match (getEventType e.FullPath) with
-        | EventType.Directory -> ()
-        | EventType.File -> uploadFile e.FullPath w.map.ShareName
-    ))
-     w.fs.Created.Add((fun e ->
-        printFunc "Created" e
-        match (getEventType e.FullPath) with
-        | EventType.Directory -> createDirectory e.FullPath w.map
-        | EventType.File -> uploadFile e.FullPath w.map.ShareName
-      ))
-     w.fs.Deleted.Add((fun e -> 
-        printFunc "Deleted" e
-      ))
-        ))
+         let event = new Event<SynchronizingEvent>()
+         let syncTriggeredEvents = event.Publish |> Observable.map(fun e -> {event = e; eventType = EventType.Ignore; time = DateTime.Now})
+         let f = syncLoop w event
+         
+            
+         let renamedEventStream = w.fs.Renamed |> Observable.map(fun(e) -> {event = {Filename = e.FullPath}; eventType = EventType.Renamed; time = DateTime.Now})
+         let changedEventStream = w.fs.Changed |> Observable.map(fun(e) -> {event = {Filename = e.FullPath}; eventType = EventType.Changed; time = DateTime.Now})
+         let createdEventStream = w.fs.Created |> Observable.map(fun(e) -> {event = {Filename = e.FullPath}; eventType = EventType.Added; time = DateTime.Now})
+         let deletedEventStream = w.fs.Deleted |> Observable.map(fun(e) -> {event = {Filename = e.FullPath}; eventType = EventType.Deleted; time = DateTime.Now})
 
-    directoryWatchers |> Seq.iter(( fun w ->
-        let f = syncLoop w
-        Async.RunSynchronously f
+         let combinedStream = 
+            syncTriggeredEvents 
+            |> Observable.merge renamedEventStream
+            |> Observable.merge changedEventStream 
+            |> Observable.merge createdEventStream
+            |> Observable.merge deletedEventStream
+         
+         combinedStream |> Observable.scan(filter) [] |> Observable.subscribe(fun l -> 
+            l |> Seq.iter (fun e -> (
+                printFunc e
+                match e.eventType with 
+                | EventType.Added ->
+                    match (getItemType e.event.Filename) with
+                    | ItemType.Directory -> createDirectory e.event.Filename w.map
+                    | ItemType.File -> uploadFile e.event.Filename w.map.ShareName
+                | EventType.Changed ->             
+                    match (getItemType e.event.Filename) with
+                    | ItemType.Directory -> ()
+                    | ItemType.File -> uploadFile e.event.Filename w.map.ShareName |> ignore
+                | EventType.Deleted -> ()
+                | EventType.Renamed -> ()
+                | EventType.Ignore -> ()
+            ))
+         ) |> ignore
+      
+         Async.RunSynchronously f
     ))
+
     
     Console.ReadLine()
     0 // return an integer exit code
---
+++ AzureFileSync/Script1.fsx
@@ -0,0 +1,39 @@
+﻿open System
+
+type EventAction = Nothing | Something
+type TimeEvent = {time: DateTime; Id: int; Action : EventAction}
+
+let t,t2 =  new System.Timers.Timer(1000.0), new System.Timers.Timer(5000.0)
+
+
+let timer1Stream  = t.Elapsed |> Observable.map(fun _ -> {time = DateTime.Now; Id = 1; Action = EventAction.Something})
+let timer2Stream  = t2.Elapsed |> Observable.map(fun _ -> {time = DateTime.Now; Id = 1; Action = EventAction.Nothing})
+
+
+let mergedStream = Observable.merge timer1Stream timer2Stream 
+
+// If an EventAction.Nothing flows through the all subsequent Events with the same Id as the EventAction.Nothing will be filtered out, until the next EventAction.Nothing
+let filter l e = 
+    printfn "filter func says: %A List contains:  %A" e l
+    let res = l |> Seq.filter(fun i -> i.Action = EventAction.Nothing) |> Seq.toList
+    match e.Action with
+    | EventAction.Nothing -> 
+        match res |> Seq.exists(fun i -> i.Id = e.Id) with
+        | true -> res|>Seq.filter(fun i -> i.Id <> e.Id) |> Seq.toList
+        | false -> e::res
+    | EventAction.Something -> 
+        match res |> Seq.exists(fun i -> i.Id = e.Id) with
+        | true -> res
+        | false -> e::res
+
+let result = 
+    mergedStream 
+    |> Observable.scan(filter) [] 
+    |> Observable.map(fun e -> e |> Seq.filter(fun i -> i.Action= EventAction.Something)) 
+    |> Observable.subscribe(fun e -> printfn "result: %A" e)
+
+t.Start()
+t2.Start()
+
+t.Stop()
+t2.Stop()
